6264fbe472c5d5f389aac1e8b6b97bf2f91e9d12
Andrew Kelly <andrewkelly692@gmail.com>
2026-02-09 19:22:17 +0100

    Remove WhatsApp and Pinterest share links

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,6 @@ import type { Props } from "astro";
 import IconMail from "@/assets/icons/IconMail.svg";
 import IconGitHub from "@/assets/icons/IconGitHub.svg";
 import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
-import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import { SITE } from "@/config";
 
 interface Social {
@@ -33,19 +32,7 @@ export const SOCIALS: Social[] = [
   },
 ] as const;
 
-export const SHARE_LINKS: Social[] = [
-  {
-    name: "WhatsApp",
-    href: "https://wa.me/?text=",
-    linkTitle: `Share this post via WhatsApp`,
-    icon: IconWhatsapp,
-  },
-  {
-    name: "Pinterest",
-    href: "https://pinterest.com/pin/create/button/?url=",
-    linkTitle: `Share this post on Pinterest`,
-    icon: IconPinterest,
-  },
+export const SHARE_LINKS: Social[] = [  
   {
     name: "Mail",
     href: "mailto:?subject=See%20this%20post&body=",
